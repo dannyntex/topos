3349c0b4f9a2e780561b8b5ebf62937971a7efb7
Jose Pedro Valiente <josepedro.valiente@gmail.com>
2024-09-26 16:30:56 +0100

    feat(fonts): Añadido iconos de line-awesome

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
+import '@/fonts/line-awesome-1.3.0/css/line-awesome.css';
 import './globals.css';
 
 const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
+  src: '../fonts/GeistVF.woff',
   variable: '--font-geist-sans',
   weight: '100 900',
 });
 const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
+  src: '../fonts/GeistMonoVF.woff',
   variable: '--font-geist-mono',
   weight: '100 900',
 });
